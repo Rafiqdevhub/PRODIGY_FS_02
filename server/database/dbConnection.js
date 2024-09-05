3ed60c392523251d0bd7e5f136596a399c085e83
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-05 07:44:07 +0500

    Initialize the backend

---
+++ server/database/dbConnection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const dbConnection = mysql.createConnection({
+  host: Process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.BD_NAME,
+});
+
+dbConnection.connect(function (err) {
+  if (err) {
+    console.log("connection error");
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+module.exports = dbConnection;
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("Public"));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
