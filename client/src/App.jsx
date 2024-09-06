80916de8e6411e78b2865a171b1bbba1cf65c485
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-06 08:55:31 +0500

    finished the ui for employee and admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,15 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import PrivateRoute from "./pages/PrivateRoute";
 import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import Employee from "./components/Employee";
+import Category from "./components/Category";
+import Profile from "./components/Profile";
+import AddCategory from "./components/AddCategory";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
+import EmployeeLogin from "./components/EmployeeLogin";
+import EmployeeDetail from "./components/EmployeeDetail";
 
 const router = createBrowserRouter([
   {
@@ -20,12 +29,50 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
+    path: "/employee_login",
+    element: <EmployeeLogin />,
+  },
+  {
+    path: "/employee_detail/:id",
+    element: <EmployeeDetail />,
+  },
+  {
     path: "/dashboard",
     element: (
       <PrivateRoute>
         <Dashboard />,
       </PrivateRoute>
     ),
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard/employee",
+        element: <Employee />,
+      },
+      {
+        path: "/dashboard/category",
+        element: <Category />,
+      },
+      {
+        path: "/dashboard/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/dashboard/add_category",
+        element: <AddCategory />,
+      },
+      {
+        path: "/dashboard/add_employee",
+        element: <AddEmployee />,
+      },
+      {
+        path: "/dashboard/edit_employee/:id",
+        element: <EditEmployee />,
+      },
+    ],
   },
 ]);
 
