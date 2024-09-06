af5cff0bc4d882c24b5ce4c8527f4dfd74c1a8ff
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-09-06 06:55:57 +0500

    Add middleware for user verification

---
+++ server/middleware/verify.js
@@ -0,0 +1,16 @@
+const Jwt = require("jsonwebtoken");
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    Jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+      if (err) return res.json({ Status: false, Error: "Wrong Token" });
+      req.id = decoded.id;
+      req.role = decoded.role;
+      next();
+    });
+  } else {
+    return res.json({ Status: false, Error: "Not autheticated" });
+  }
+};
+
+module.exports = verifyUser;
